bb8b52d82c69c639307ac244da703cfbbc8905f9
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2024-05-13 10:44:47 +0700

    fungsi utama untuk set nama setiap mahasiswa

--- StaticMemberVariable/StaticMemberVariable.cpp
+++ StaticMemberVariable/StaticMemberVariable.cpp
@@ -32,6 +32,15 @@ void mahasiswa::setId()
 void mahasiswa::printAll()
 {
 	cout << "ID = " << id << endl;
-	cout << "Nama = " << nama << < endl;
+	cout << "Nama = " << nama <<  endl;
 	cout << endl;
+}
+
+int main()
+{
+	mahasiswa mhs1("Lia Kurnia");
+	mahasiswa mhs2("Asroni");
+	mahasiswa mhs3("Andi Kurniawan");
+	mahasiswa mhs4("Joko Purbo");
+
 }
