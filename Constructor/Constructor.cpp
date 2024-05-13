bf40b3ccb09929679c02a022a50aff3a5dab30ee
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2024-05-13 11:33:44 +0700

    construktor

--- Constructor/Constructor.cpp
+++ Constructor/Constructor.cpp
@@ -3,12 +3,36 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa
+class Mahasiswa
 {
 public:
 	int nim;
 	string nama;
 
+public:
+	Mahasiswa()
+	{
+		nim = 0;
+		nama = "";
 
+	};
+	Mahasiswa(int iNim)
+	{
+		nim = iNim;
+	}
+	Mahasiswa(string iNama)
+	{
+		nama = iNama;
+	}
+	Mahasiswa(int iNim, string iNama)
+	{
+		nim = iNim;
+		nama = iNama;
+	}
+	void cetak()
+	{
+		cout << endl << "nim =" << nim << endl;
+		cout << "Nama = " << nama << endl;
+	}
 };
 
